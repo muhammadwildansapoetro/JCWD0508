65dd000767b72acedabbf962c24e8086c408ceea
muhammadwildansapoetro <muhwilsap@gmail.com>
2024-10-04 10:09:11 +0700

    session-4

---
+++ session-4/exercise/array.ts
@@ -0,0 +1,16 @@
+
+// cara pertama membuat array
+
+const arr = ["1, 2, 3"]
+
+console.log(arr)
+
+// arry = tipe data yang bisa menyimpan banyak data
+// setiap data dipisah dengan koma (,)
+// dikurung dengan square bracket
+
+// cara kedua buat array
+
+const arr2 = new Array(1, 2, 3)
+
+console.log(arr2)
